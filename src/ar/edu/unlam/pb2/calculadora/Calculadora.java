89bebf1dcee7c5bb881cec11596fa7a6d70521b7
Eduar Chilon <echiloncruz@alumno.unlam.edu.ar>
2020-09-26 20:30:48 -0300

    Segundo commit - cambio de if/else por un switch

--- src/ar/edu/unlam/pb2/calculadora/Calculadora.java
+++ src/ar/edu/unlam/pb2/calculadora/Calculadora.java
@@ -1,28 +1,44 @@
 package ar.edu.unlam.pb2.calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
 
 	public static void main(String[] args) {
 		Integer a;
 		Integer b;
 		Integer opcion;
-		Integer resultado;
+		Integer resultado=0;
+		
+		Scanner teclado  = new Scanner(System.in);
+		
 		do{
-		System.out.println("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
-		opcion = teclado.next();
-		}while(opcion < 1 && opcion > 4)
+			System.out.println("INGRESE SU OPERACIÓN:");
+			System.out.println("1 - Sumar");
+			System.out.println("2- Restar");
+			System.out.println("3- Multiplicar");
+			System.out.println("4- Dividir");
+			opcion = teclado.nextInt();
+		}while(opcion <= 1 && opcion >= 4);
 		System.out.println("Ingrese el primer numero");
-		a = teclado.next();
+		a = teclado.nextInt();
 		System.out.println("Ingrese el segundo numero");
-		a = teclado.next();
-		if(opcion.equals(1)){
-		resultado = a + b;
-		}else if(opcion.equals(2)){
-		resultado = a - b;
-		}else if(opcion.equals(3)){
-		resultado = a * b;
-		}else{
-		resultado = a / b;
+		b = teclado.nextInt();
+		switch(opcion) {
+		case 1:
+			resultado = a+b;
+			break;
+		case 2:
+			resultado = a-b;
+			break;
+		case 3:
+			resultado = a*b;
+			break;
+		case 4:
+			resultado = a/b;
+			break;
+		default:
+			break;
 		}
 		System.out.println("El resultado es " + resultado.toString());
 		}
